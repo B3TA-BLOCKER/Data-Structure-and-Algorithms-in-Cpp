35d6dc484368d8ae4e531b125db3db13103e8410
Hassaan Ali Bukhari <112207209+B3TA-BLOCKER@users.noreply.github.com>
2023-12-22 17:13:50 +0500

    Add files via upload

---
+++ Binary Tree/Level_Order_Traversal.cpp
@@ -0,0 +1,116 @@
+// Level Order is implemented via QUEUE
+
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
+
+    node(int data)
+    {
+        this->data = data;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+class Binary_Tree
+{
+private:
+    node *root;
+
+    node *Insert(node *Node, int data)
+    {
+        if (Node == NULL)
+        {
+            return new node(data);
+        }
+
+        if (data < Node->data)
+        {
+            Node->left = Insert(Node->left, data);
+        }
+        else if (data > Node->data)
+        {
+            Node->right = Insert(Node->right, data);
+        }
+
+        return Node;
+    }
+
+    void Level_Order(node *root)
+    {
+        queue<node *> q;
+
+        q.push(root);
+        q.push(NULL);
+
+        while (!q.empty())
+        {
+            node *Node = q.front();
+            if (Node != NULL)
+            {
+                cout << Node->data << " ";
+                if (Node->left != NULL)
+                {
+                    q.push(Node->left);
+                }
+                if (Node->right != NULL)
+                {
+                    q.push(Node->right);
+                }
+            }
+            else
+            {
+                q.push(NULL);
+            }
+            q.pop();
+        }
+    }
+
+public:
+    Binary_Tree()
+    {
+        root = NULL;
+    }
+
+    void insert_node(int data)
+    {
+        root = Insert(root, data);
+    }
+
+    void Print_Tree()
+    {
+        Level_Order(root);
+        cout << endl;
+    }
+};
+
+int main(void)
+{
+    Binary_Tree tree;
+
+    int num, node;
+
+    cout << "Enter How Many Number You Want To Enter : " << endl
+         << " -> ";
+    cin >> num;
+
+    cout << endl
+         << "Enter The Numbers" << endl;
+    for (int i = 0; i < num; i++)
+    {
+        cin >> node;
+        tree.insert_node(node);
+    }
+
+    cout << endl
+         << "This Is The Tree Formed " << endl;
+    tree.Print_Tree();
+}
